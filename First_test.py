8ecd7f0c61691225629ab5f1571752d2f52f86e3
Tollytweety <motolasarahog@gmail.com>
2025-03-29 11:08:04 +0100

    push first_test

--- First_test.py
+++ First_test.py
@@ -3,12 +3,18 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-driver.maximize_window()
-time.sleep(5)
+wait = 5
+url = "https://www.saucedemo.com/"
+username = "standard_user"
+password ="secret_sauce"
 
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
+
+driver = webdriver.Chrome()
+driver.get(url)
+driver.maximize_window()
+time.sleep(wait)
+
+driver.find_element(By.ID, "user-name").send_keys(username)
+driver.find_element(By.ID, "password").send_keys(password)
 driver.find_element(By.ID, "login-button").click()
-time.sleep(5)
+time.sleep(wait)
